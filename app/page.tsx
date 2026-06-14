ea15db3f0748983824bd365a50b31e195dcdeb1e
reber90 <40600775+reber90@users.noreply.github.com>
2026-06-14 15:25:02 +0300

    Delete app directory

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-// The site opens straight on the inventory, no separate landing page
-export default function HomePage() {
-  redirect('/inventory')
-}
